6f38eb44689a5b6e075a3a72122164d633f44c92
Computer Science Society Club at John Jay College <33881291+jjcss@users.noreply.github.com>
2022-01-26 00:41:19 -0500

    Delete js directory

--- js/index.js
+++
@@ -1,8 +0,0 @@
-// Navbar JS
-const toggleButton = document.getElementsByClassName("toggle-button")[0]
-const navbarLinks = document.getElementsByClassName("navbar-links")[0]
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle("active")
-})
-// 
